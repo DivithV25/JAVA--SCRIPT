04e331fdb7dfcbaf76f1ea47e63db6359ed2f626
DivithV25 <divithv25@gmail.com>
2024-04-16 17:52:10 +0530

    variables and objects

---
+++ 1.js
@@ -0,0 +1,29 @@
+console.log("new java script file")
+
+var a = 10;// var is used to define variable but it even changes the value in the block
+console.log(a)
+{
+ var a = 20;
+ console.log(a)
+}
+console.log(a)
+
+let b = 20; // let is used to define variable but it doesnot changes the value in block
+console.log(b)
+{
+    
+    let b =30;
+    console.log(b)
+
+}
+console.log(a)
+
+//object
+let o={
+ "name":"java",
+ "code number":1212
+
+}
+console.log(o)
+o.salary="1 crore"
+console.log(o)
