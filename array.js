a52b065459439dbcfdc33f0ef2da84a7260ce86e
DivithV25 <divithv25@gmail.com>
2024-04-24 19:24:18 +0530

    arrays 1

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-let arr = [1,2,4,6]
+let arr = [1, 2, 4, 6]
 
 console.log(arr);
 console.log(arr.length);
@@ -15,26 +15,26 @@ console.log(arr.shift())
 console.log(arr.unshift("jack"))
 console.log(arr.length)
 // console.log(arr.sort())
-let num = [1,2,3,4,5]
-num.slice(1,3)
-for(let index=0;index<arr.length;index++){
+let num = [1, 2, 3, 4, 5]
+num.slice(1, 3)
+for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
     console.log(element)
 }
 
-let arr1 = [1,2,4,6]
-arr1.forEach((value,index,arr1)=>{
-    console.log(value,index,arr1)
+let arr1 = [1, 2, 4, 6]
+arr1.forEach((value, index, arr1) => {
+    console.log(value, index, arr1)
 })
 
-let obj={
-  "a":"1",
-  "b":"2",
-  "c":"3"
+let obj = {
+    "a": "1",
+    "b": "2",
+    "c": "3"
 }
 for (const key in obj) {
     if (Object.hasOwnProperty.call(obj, key)) {
         const element = obj[key];
         console.log(element)
-        }
+    }
 }
